40b09a7b5502ce2d82900dd4c9c11aa43e80a788
maldoa21 <121905184+maldoa21@users.noreply.github.com>
2025-04-24 23:04:21 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,16 +1,10 @@
 from typing import List, Optional
 
-import pip._internal.utils.inject_securetransport  # noqa
-from pip._internal.utils import _log
-
-# init_logging() must be called before any call to logging.getLogger()
-# which happens at import of most modules.
-_log.init_logging()
+__version__ = "23.0.1"
 
 
-def main(args: (Optional[List[str]]) = None) -> int:
-    """This is preserved for old console scripts that may still be referencing
-    it.
+def main(args: Optional[List[str]] = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
 
     For additional details, see https://github.com/pypa/pip/issues/7498.
     """
